c74d4992d955ba454dfb2cea604a11c9aeb13735
amod <amod.kadam@cloudmanthan.com>
2018-06-15 16:31:53 +0530

    one more change

--- AWSSamples/src/com/cloudmanthan/aws/cognito/CognitoTest.java
+++ AWSSamples/src/com/cloudmanthan/aws/cognito/CognitoTest.java
@@ -5,6 +5,8 @@ public class CognitoTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Cognito test");
+		System.out.println("Cognito test");
+			
 	}
 
 }
